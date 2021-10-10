6269ba09763021d2a79fdafdd9e8c8a9e5443236
Kai Hoenshell <kai.hoenshell@geappliances.com>
2021-10-10 05:23:54 -0400

    1

--- IdentifierTriggerFile.py
+++ IdentifierTriggerFile.py
@@ -2,14 +2,6 @@ import practice
 import subprocess
 import os
 
-# gitpush = subprocess.Popen(["./gitpush.sh"], "/Users/kaihoenshell/git/MotionPush", stdin=subprocess.PIPE)
-
 if(practice.x > 1):
 
     subprocess.call("./gitpush.sh", shell=True)
-
-
-
-    # gitpush.stdin.write("yes\n")
-    # gitpush.stdin.close()
-    # returncode = gitpush.wait()
